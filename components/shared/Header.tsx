6f30a65b393a08277f232c2e7d159bb24eff4e53
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2025-08-26 14:18:56 +0100

    feat(header): update header component with logo, navigation and buttons
    - Add HexPay logo image
    - Implement navigation links from utils/data
    - Add Get Started and Book a Demo buttons
    - Remove unused SVG assets

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,13 +1,37 @@
 import React from "react";
+import Image from "next/image";
+import { navLinks } from "@/utils/data";
+import { Button } from "../ui/button";
+import { ArrowUpRight, CirclePlay } from "lucide-react";
 
 const Header = () => {
   return (
     <header>
-      <nav>
-        <div className="container">
-          <div className="flex justify-between items-center">
-            <div className="text-2xl font-bold">HexPay</div>
-          </div>
+      <nav className="container flex items-center justify-between">
+        <Image
+          className="dark:invert"
+          src="/images/hexpay.png"
+          alt="HexPay logo"
+          width={94}
+          height={49.46371078491211}
+          priority
+        />
+        <ul className="flex items-center gap-8">
+          {navLinks.map((item) => (
+            <li key={item.name}>
+              <a href={item.link}>{item.name}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="flex">
+          <Button variant="outline">
+            <ArrowUpRight />
+            Get Started
+          </Button>
+          <Button>
+            <CirclePlay />
+            Book a Demo
+          </Button>
         </div>
       </nav>
     </header>
