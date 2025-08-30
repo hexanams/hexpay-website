8d7efb8d0aa47b5c8593b7e451b67172ce52aa30
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2025-08-30 06:03:52 +0100

    feat(styles): add custom color variables and update header styles
    - Add new CSS variables for custom dark and purple colors
    - Apply new color variables to header components
    - Adjust header layout and spacing

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 const Header = () => {
   return (
     <header className="w-full flex items-center justify-center">
-      <nav className="container flex items-center justify-between">
+      <nav className="container flex items-center justify-between h-20">
         <Image
           className="dark:invert"
           src="/images/hexpay.png"
@@ -17,24 +17,27 @@ const Header = () => {
           height={49.46371078491211}
           priority
         />
-        <ul className="flex items-center gap-8">
+        <ul className="flex items-center gap-3">
           {navLinks.map((item) => (
             <li key={item.name}>
               <Link
                 href={item.link}
-                className="font-aspekta font-normal text-base"
+                className="font-aspekta font-normal text-base p-3 text-custom-dark"
               >
                 {item.name}
               </Link>
             </li>
           ))}
         </ul>
-        <div className="flex">
-          <Button variant="outline">
-            <ArrowUpRight />
+        <div className="flex gap-[23px]">
+          <Button
+            variant="outline"
+            className="border-2 border-custom-purple rounded-[30px] font-aspekta font-normal text-sm text-custom-purple"
+          >
+            <ArrowUpRight className="text-custom-purple" />
             Get Started
           </Button>
-          <Button>
+          <Button className=" bg-custom-purple rounded-[30px] font-aspekta font-normal text-sm text-white">
             <CirclePlay />
             Book a Demo
           </Button>
